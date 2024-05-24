a4c33f5b4edced798770f7c4a69992a71faa4a09
Baruch R. <azulamarillo000@gmail.com>
2024-05-24 11:01:55 -0600

    Los conteos N _v y N_vnc se realizan en conteos.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 from PyPostQL import pruebasPostgres
 from Var_Clss_Construction import dict_construction, df_construction
 from tree_variables_from_db import creacion_ramas_arbol
+import conteos
 from config import fuente_de_datos_metadatos
 
 app = Flask(__name__)
@@ -56,13 +57,16 @@ def select_variables():
 #### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ####
 
 #### Construcción DataFrame de variables y clase ####
+
     df_all_variables_data = df_construction(dict_db_variables, 'Covariable')
     df_all_class_data = df_construction(dict_db_class, 'clase')
 
-    df_all_variables_data['N_v'] = list(map(lambda x: len(set(x)), df_all_variables_data.iloc[:, -1]))
-    df_all_variables_data['N_vnc'] = [conteo_interseccion(x, list(df_all_class_data.celdas)[0]) for x in list(df_all_variables_data.celdas)]
-
+    # Extraemos el nombre de la clase
     nombre_clase = df_all_class_data.iloc[0, 0]
+#### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ####
+
+#### Contamos el número de celdas de variables y clase ####
+    conteos.df_count_cells(df_all_variables_data, df_all_class_data)    # Esta línea modifica la tabla original df_all_variables_data
 
     # Creamos un nuevo DataFrame con las celdas desanidadas.
     df_all_cells_data = df_all_variables_data.explode('celdas')
@@ -80,10 +84,7 @@ def select_variables():
     return render_template('resDf.html', 
                            df_resultado = df_all_variables_data.drop(['celdas'],axis=1).to_html(), 
                            df_resultado2 = df_all_cells_data.to_html(escape=False),
-                           nombre_titulo=nombre_clase)
-
-def conteo_interseccion(l_var, l_cov):
-    return sum(1 for var in l_var if var in l_cov)
+                           nombre_titulo = nombre_clase)
 
 if __name__ == '__main__':
     app.run(debug=True)
