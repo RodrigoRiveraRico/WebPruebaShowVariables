ade94ea429120409f400c06f2b13889b2af16ed8
Baruch R. <azulamarillo000@gmail.com>
2024-06-03 14:06:36 -0600

    Se agregan comentarios

--- Resolucion.py
+++ Resolucion.py
@@ -1,7 +1,13 @@
 from config import fuente_de_datos_metadatos
 
 def ditc_res_DBs_list(bases):
-    dic = {}    # Diccionario a construir. Resolucion : [base de datos]
+    '''
+    bases : lst Lista de bases de datos
+
+
+    Return : dict {'resolución1' : [DB1, DB2,], 'resolución2' : [DB1, DB3,],}
+    '''
+    dic = {}    # Diccionario a construir.
     
     for db in bases:    # Para cada base de las seleccionadas
         res_dict = fuente_de_datos_metadatos[db]['resolution']  # Se asigna el diccionario de resolucion de cada base
@@ -9,5 +15,5 @@ def ditc_res_DBs_list(bases):
             if key not in dic:  # Si la resolucion no está en el diccionario
                 dic.update({key:[]})    # Agregamos la resolución faltante cuyos valores será una lista de bases de datos
             dic[key].append(db) # Añadimos la base de datos a la resolución
-
+    print(dic)
     return dic
