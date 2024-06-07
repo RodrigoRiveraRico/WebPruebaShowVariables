ee3f05d63d4bb8917e70a5d40f4ebadce8a6c84f
Baruch R. <azulamarillo000@gmail.com>
2024-06-06 23:39:13 -0600

    Se añade una prueba con una lista de strings

--- prueba.py
+++ prueba.py
@@ -2,19 +2,20 @@ import re
 import pandas as pd
 
 txt = "{1,'200',3,4,5,6,77,23,222,1}"
+lista  = ['1','23','200','100']
 
-x = re.findall('\d+',txt)
+x = re.findall('\d+',str(txt))
 
 print(x)
 
-df = pd.read_csv('./New_DB_Personas.csv')
+# df = pd.read_csv('./New_DB_Personas.csv')
 
-print(df)
+# print(df)
 
-def encontrar(row):
-    ls = re.findall('\d+',row['presencias'])
-    return ls
+# def encontrar(row):
+#     ls = re.findall('\d+',row['presencias'])
+#     return ls
 
-df['lista'] = df.apply(encontrar, axis=1)
+# df['lista'] = df.apply(encontrar, axis=1)
 
-print(df['lista'].to_numpy())
+# print(df['lista'].to_numpy())
