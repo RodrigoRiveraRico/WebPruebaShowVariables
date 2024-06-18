b522a61aa4d62146ce3c7795d26a9ae0e11c4ebb
Baruch R. <azulamarillo000@gmail.com>
2024-06-18 14:43:25 -0600

    Recodificación creación del árbol. Implementación sqlalchemy.

--- app/tree_variables_from_db.py
+++ app/tree_variables_from_db.py
@@ -1,7 +1,7 @@
-import psycopg2
 import pandas as pd
 from app.arbol_function import arbol
 from flask import current_app
+from sqlalchemy import create_engine, text
 
 # Función para contar el número de elementos separados por coma
 def count_elements(metadata):
@@ -12,7 +12,7 @@ def count_elements(metadata):
     '''
     return len(metadata.split(','))
 
-def creacion_ramas_arbol(DB:str):
+def creacion_ramas_arbol(DB: str):
     '''
     DB : str Nombre de la base de datos
     
@@ -20,116 +20,72 @@ def creacion_ramas_arbol(DB:str):
     '''
     fuente_de_datos_metadatos = current_app.config['FUENTE_DE_DATOS_METADATOS']
     query_categorias = current_app.config['QUERY_CATEGORIAS']
-    conn = psycopg2.connect(database = DB,
-                            user = fuente_de_datos_metadatos[DB]['user'],
-                            host = fuente_de_datos_metadatos[DB]['host'],
-                            password = fuente_de_datos_metadatos[DB]['password'],
-                            port = fuente_de_datos_metadatos[DB]['port'])
+    user = fuente_de_datos_metadatos[DB]['user']
+    host = fuente_de_datos_metadatos[DB]['host']
+    password = fuente_de_datos_metadatos[DB]['password']
+    port = fuente_de_datos_metadatos[DB]['port']
+    database = DB
+    database_url = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
 
-    sql_query = query_categorias[DB]
+    engine = create_engine(database_url)
 
-    df = pd.read_sql(sql_query, conn)
+    sql_query = text(query_categorias[DB])
 
-    conn.close()
+    with engine.connect() as connection:
+        df = pd.read_sql(sql_query, connection)
 
-    ### Importante ordenar las varibales de forma ascendente al número de niveles en el que están ubicadas.
-    ### El diccionario (árbol) se crea guardando las variables de más internas a más externas.
     # Crear una nueva columna temporal con el conteo de elementos.
     df['element_count'] = df['metadatos'].apply(count_elements)
 
     # Ordenar el DataFrame
     df = df.sort_values(
-        by=['element_count', 'metadatos', 'nombre_variable', 'intervalo']   # Hay que ordenar primero por 'element_count'.
-        ).drop(columns=['element_count'])  # Eliminar la columna temporal después de ordenar.
+        by=['element_count', 'metadatos', 'nombre_variable', 'intervalo']  # Ordenar primero por 'element_count'.
+    ).drop(columns=['element_count'])  # Eliminar la columna temporal después de ordenar.
 
-    # df = df.head(100)   # Solo consideramos 100 datos de cada base de datos
-
-    path_dict = arbol(df)   # Quizá un mejor nombre sería variables_categories
-                            # Al usar la función arbol ya no aparecen nombres de variables repetidas, si es que las hay.
+    path_dict = arbol(df)
 
     structure_lst = []
 
-    for path_key in path_dict.keys():   # Quizá un mejor nombre para path_key sería categorías o levels
+    def add_node(id_tag, text, children_list):
+        return {'id': id_tag, 'text': text, 'children': children_list}
+
+    def find_or_create_node(structure, id_tag, text):
+        for node in structure:
+            if node['id'] == id_tag:
+                return node
+        new_node = add_node(id_tag, text, [])
+        structure.append(new_node)
+        return new_node
+
+    for path_key, value in path_dict.items():
         key_list = path_key.split(', ')
-        id_tag = DB + ' '   # Id de cada rama antes de llegar a la rama de variables.
-        for key_idx in range(len(key_list)):
-            id_tag += key_list[key_idx] + ' '   # El id se va construyendo por cada nivel.
-            if key_idx == 0:
-                if structure_lst == []:
-                    structure_lst.append({'id': id_tag,
-                                          'text': key_list[key_idx],
-                                          'children':[]})
-                    idx = 0
-                else:
-                    for idx, d in enumerate(structure_lst):
-                        if d['id'] != id_tag and idx == len(structure_lst) - 1:
-                            structure_lst.append({'id': id_tag,
-                                                  'text': key_list[key_idx],
-                                                  'children':[]}) # Al hacer este append, añadimos una vuelta más al ciclo for         
-                        elif d['id'] == id_tag:
-                            break
-                structure_child_lst = 'structure_lst' + str([idx]) + str(['children'])
+        id_tag = DB + ' '
+        current_structure = structure_lst
 
-            elif key_idx <= len(key_list) - 1:
-                if eval(structure_child_lst) == []:
-                    eval(structure_child_lst).append({'id': id_tag,
-                                                      'text': key_list[key_idx],
-                                                      'children':[]})
-                    idx = 0
-                else:
-                    for idx, d in enumerate(eval(structure_child_lst)):
-                        if d['id'] != id_tag and idx == len(eval(structure_child_lst)) - 1:
-                            eval(structure_child_lst).append({'id': id_tag,
-                                                              'text': key_list[key_idx],
-                                                              'children':[]}) # Al hacer este append, añadimos una vuelta más al ciclo for   
-                        elif d['id'] == id_tag:
-                            break    
-                structure_child_lst += str([idx]) + str(['children'])
+        for key_idx, key in enumerate(key_list):
+            id_tag += key + ' '
+            node = find_or_create_node(current_structure, id_tag, key)
+            current_structure = node['children']
 
-        # En el siguiente append agregamos el nivel donde se almecanarán las variables.
-        eval(structure_child_lst).append({'id': DB + '__variables__' + path_key,
-                                          'text':'variables',
-                                          'children':[]})
-        # En las líneas siguientes se almacenan las variables dentro de este último diccionario creado.
-        # Dado que se empleó la función 'arbol(df)' para construir el diccionario path_key, las variables a almacenar no están repetidas.
+        # Agregar nodo de variables
+        variables_node = add_node(DB + '__variables__' + path_key, 'variables', [])
+        current_structure.append(variables_node)
 
-        # Queremos que el diccionario en donde se almacenarán las variables sea el primer elemento de la lista de diccionarios de structure_child_lst,
-        # por eso hicimos el acomodo de  los niveles de menos profundo a más profundo.
-        structure_child_lst += str([0]) + str(['children'])
+        # Agregar variables dentro del nodo de variables
+        for nombre_variable in value['__variables__']:
+            variable_node = add_node(DB + ' ' + path_key + ' ' + nombre_variable, nombre_variable, [])
+            variables_node['children'].append(variable_node)
 
-        variables_lst = path_dict[path_key]['__variables__']
-        
-        for idx, nombre_variable in enumerate(variables_lst):
-            eval(structure_child_lst).append({'id': DB + ' ' + path_key + ' ' + nombre_variable,
-                                              'text': nombre_variable,
-                                              'children' : []   # Si no incuimos los intervalos, 'children' puede quedar vacío o no existir y el código subsecuente no se incluiría.  
-                                              })
+            ser_nombre_variable = df[(df['nombre_variable'] == nombre_variable) & (df['metadatos'] == path_key)]['nombre_variable']
+            ser_intervalo = df[(df['nombre_variable'] == nombre_variable) & (df['metadatos'] == path_key)]['intervalo']
 
-            last_structure_child_lst = structure_child_lst + str([idx]) + str(['children'])
-            
-            ser_nombre_variable = df[(df['nombre_variable']==nombre_variable) & (df['metadatos']==path_key)]['nombre_variable']
-            ser_intervalo = df[(df['nombre_variable']==nombre_variable) & (df['metadatos']==path_key)]['intervalo']
-
-            for variable_intervalo in zip(ser_nombre_variable,ser_intervalo):
-                eval(last_structure_child_lst).append({'id': '__' + DB + '__' + ' ' + path_key + ' ' + str(variable_intervalo),
-                                                       'text': str(variable_intervalo[0]) + ', ' + str(variable_intervalo[1])})
-
-
-    # Obtenemos lo siguiente:
-    # {'id': DB + ___, 
-    #  'texto': ___, 
-    #  'children': [{'id': DB +'__variables__' + path_key,
-    #                'text': 'variables',
-    #                'children' : [{'id': DB + ___,
-    #                               'text': nombre_variable,
-    #                               'children':[{'id': __DB__ + ___,
-    #                                            'texto': nombre_variable + intervalo},]
-    #                             },]
-    #                 },
-    #                 {'id': DB + ___,
-    #                     'text': ___,
-    #                     'children': []},
-    #             ], 
-    # }
+            for variable_intervalo in zip(ser_nombre_variable, ser_intervalo):
+                interval_node = add_node('__' + DB + '__' + ' ' + path_key + ' ' + str(variable_intervalo),
+                                         str(variable_intervalo[0]) + ', ' + str(variable_intervalo[1]), [])
+                variable_node['children'].append(interval_node)
 
     return structure_lst
+
+# Ejemplo de uso
+# creacion_ramas_arbol('Personas')
+# creacion_ramas_arbol('epi_puma_worldclim')
