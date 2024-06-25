322c9f6325b8dbf5c09a16d92b416ebd8e443e6e
Baruch R. <azulamarillo000@gmail.com>
2024-06-25 16:15:07 -0600

    No se emplea la paquetería regex

--- app/tree_variables_from_db.py
+++ app/tree_variables_from_db.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from flask import current_app
 from sqlalchemy import create_engine, text
-import re
 
 # Función para contar el número de elementos separados por coma
 def count_elements(metadata):
