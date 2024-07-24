8fe8dd046eb87ec27894f527ab99ecf25cb5807d
Baruch R. <azulamarillo000@gmail.com>
2024-07-24 14:28:20 -0600

    primera propuesta de configuracion incluyendo sql

--- configuraciones_db/config_niche.py
+++ configuraciones_db/config_niche.py
@@ -4,6 +4,37 @@ with open('configuraciones_db/config_niche.yaml') as stream:
     plataforma = config_loaded['plataforma']
     fuente_de_datos_metadatos = config_loaded['fuente_de_datos_metadatos']
 
+def concatenacion_taxonomia(db_name:str):
+    text_concat = ''
+    for column in fuente_de_datos_metadatos[db_name]['variable_columns']:
+        text_concat += column + ', '
+    return text_concat[:-2]
+
+def concatenacion_metadatos(db_name:str):
+    text_concat = ''
+    for column in fuente_de_datos_metadatos[db_name]['categorias']['columnas']:
+        text_concat += column + ",', ', "
+    return text_concat[:-7]
+
+def table(db_name:str):
+    return fuente_de_datos_metadatos[db_name]['table']
+
+query_categorias = {}
+for db_name in fuente_de_datos_metadatos:
+    txt = f"""
+        SELECT 
+            CONCAT({concatenacion_taxonomia(db_name)}) as taxonomia_variable,
+        
+            CONCAT({concatenacion_metadatos(db_name)}) as metadatos
+
+            FROM {table(db_name)}
+        ;
+        """
+    query_categorias = {db_name : txt}
+
+print(query_categorias)
+
+
 query_categorias = {
     'niche_integration' : '''
         select concat({lab_var}) as taxonomia_variable,
@@ -14,3 +45,4 @@ query_categorias = {
     '''.format(lab_var = fuente_de_datos_metadatos['niche_integration']['variable_columns'][0],
             table = fuente_de_datos_metadatos['niche_integration']['table'])
 }
+print(query_categorias)
